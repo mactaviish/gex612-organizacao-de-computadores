cb20c602c5fee4a10ee83f89af74420b38fa29d3
Leonardo Dalla Corte <37817455+mactaviish@users.noreply.github.com>
2024-05-30 17:02:14 -0300

    Update ListaEncadeada.asm

--- ListaEncadeada/ListaEncadeada.asm
+++ ListaEncadeada/ListaEncadeada.asm
@@ -1,31 +1,39 @@
 .data
 menu_string:
-    	.asciz "\n# Menu\n#1 - Inserir elemento na lista\n#2 - Remover elemento da lista por indice\n#3 - Remover elemento da lista por valor\n#4 - Mostrar todos os elementos da lista\n#5 - Mostrar estatisticas\n#6 - Sair do programa\nEscolha: "
+    	.asciz "\n\n# Menu\n#1 - Inserir elemento na lista\n#2 - Remover elemento da lista por indice\n#3 - Remover elemento da lista por valor\n#4 - Mostrar todos os elementos da lista\n#5 - Mostrar estatisticas\n#6 - Sair do programa\nEscolha: "
 msg_inserir_valor: 
 	.asciz "\nDigite o valor:\n"
 msg_erro: 
 	.asciz "\nOcorreu um erro\n"
+msg_inserido:
+	.asciz "\nInserido com sucesso!\nIndice: "
+msg_removido_indice:
+	.asciz "\nRemovido com sucesso!\nValor: "
+msg_removido_valor:
+	.asciz "\nRemovido com sucesso!\nIndice: "
 selecao_menu:
     	.word 0
 head:
 	.word 0
 tail:
 	.word 0
+qtd_lista:
+	.word 0
 .text
 
 main:
 	#imprime menu
 menu:
-	la a0, menu_string      
-	li a7, 4               
+	la a0, menu_string
+	li a7, 4
 	ecall
 	# Le a opcao selecionada pelo usuario
-	li a7, 5                
+	li a7, 5
 	ecall
-    	la t1, selecao_menu      
-    	sw a0, 0(t1)            
+    	la t1, selecao_menu
+    	sw a0, 0(t1)
     	lw t0, 0(t1)
-	
+
     	#chama a funcao selecionada pelo usuario
    	li t1, 4
     	beq t0, t1, imprime_lista
@@ -33,7 +41,9 @@ menu:
     	beq t0, t1, estatistica
     	li t1, 6
     	beq t0, t1, sair_programa
+
 	jal ler_valor
+
     	li t1, 1
     	beq t0, t1, inserir_inteiro
     	li t1, 2
@@ -43,25 +53,31 @@ menu:
  	#caso alguma opcao invalida seja selecionada imprime o menu novamente
     	j menu
 inserir_inteiro:
-	sw s0, 0(a0) #inicio da lista	
-	li a0, 8
-	li a7, 9
-	ecall #aqui vai retornar em a0 a posicao de memoria alocada
-	beq s0, zero, tratar_lista_vazia
-	#lw s1, tail	
-	#sw s1, 0(t5)
-	#sw a1, 0(s1)
-	#sw zero, 4(s1)
-    	li t6, -1
-    	j verifica_retorno
-tratar_lista_vazia:
 	mv s0, a0
-	la t4, head
-	la t5, tail
-	sw s0, 0(t4)
+	jal malloc_8
+
+	lw s0, 0(s0)
+	beq s0, zero, lista_vazia
+
+	lw t1, tail #endereco ultimo
+	sw a0, 4(t1) #ultimo aponta para o novo
+	sw a1, 0(a0) #salva o valor do novo
+	sw zero, 4(a0) #novo aponta para null
+	la t1, tail #endereco ultimo
+	sw a0, 0(t1) #novo ultimo
+
+	jal atualiza_qtd
+    	j verifica_retorno
+lista_vazia:
+	mv s0, a0
+	la t0, head
+	la t1, tail
+	sw s0, 0(t0)
+	sw s0, 0(t1)
 	sw a1, 0(s0)
 	sw zero, 4(s0)
-	li t6, 0 #indice retornado na funcao 
+
+	jal atualiza_qtd
 	j verifica_retorno
 remover_por_indice:
     	# ...
@@ -72,27 +88,68 @@ remover_por_valor:
     	mv t6, a1
     	j verifica_retorno
 imprime_lista:
-    	# ...
+    	la t0, head
+laco_imprecao:
+	beq t0, zero, fim_laco
+	lw t1, 0(t0)
+	
+	li a7, 1
+	mv a0, t1
+	ecall
+	
+#	add zero, zero, zero
+
+    	addi t0, t0, 4
+    	j laco_imprecao
+fim_laco:
     	j menu
 estatistica:
     	# ...
     	j menu
 ler_valor:
-	la a0, msg_inserir_valor      
-	li a7, 4               
+	la a0, msg_inserir_valor
+	li a7, 4
 	ecall
-	# Le a opcao selecionada pelo usuario
-	li a7, 5                
+	li a7, 5
 	ecall
 	mv a1, a0
 	la a0, head
 	ret
+malloc_8:
+	li a0, 8
+	li a7, 9
+	ecall
+	ret
+atualiza_qtd:
+	lw t6, qtd_lista
+	addi t6, t6, 1
+	la t1, qtd_lista
+	sw t6, 0(t1)
+	ret
 verifica_retorno:
 	blt t6, zero, retorno_com_erro
+	lw t0, selecao_menu
+	li t1, 1
+	beq t0, t1, inserido
+	li t1, 2
+	beq t0, t1, removido_indice
+	li t1, 3
+	beq t0, t1, removido_valor
+inserido:
+	la a0, msg_inserido
+	j executa_retorno
+removido_indice:
+	la a0, msg_removido_indice
+	j executa_retorno
+removido_valor:
+	la a0, msg_removido_valor
+executa_retorno:
+	li a7, 4
+	ecall
 	mv a0, t6
 	li a7, 1
 	ecall
-	j menu	
+	j menu
 retorno_com_erro:
 	la a0, msg_erro      
 	li a7, 4               
